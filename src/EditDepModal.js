21980009f86e379f488c1606f8b60197fb4294d0
Employees <GerardoHerrera20@hotmail.com>
2022-07-10 00:05:51 -0500

    Agregar modal para Edit y boton borrar

---
+++ src/EditDepModal.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import { Modal, Button, Row, Col, Form } from "react-bootstrap";
+
+export class EditDepModal extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  //enviar la información almacenadad en el modal, agregar un department
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(process.env.REACT_APP_API + "/Department", {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        DepartmentId: event.target.DepartmentId.value,
+        DepartmentName: event.target.DepartmentName.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          alert(result);
+        },
+        (error) => {
+          alert("Failed");
+        }
+      );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Modal
+          {...this.props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter">
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Edit Department
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Row>
+              <Col sm={6}>
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Group controlId="DepartmentId">
+                    <Form.Label>DepartmentId</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="DepartmentId"
+                      required
+                      disabled
+                      defaultValue={this.props.depid}
+                      placeholder="DepartmentId"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="DepartmentName">
+                    <Form.Label>DepartmentName</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="DepartmentName"
+                      required
+                      defaultValue={this.props.depname}
+                      placeholder="DepartmenName"
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Button variant="primary" type="submit">
+                      Editar Department
+                    </Button>
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="danger" onClick={this.props.onHide}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
